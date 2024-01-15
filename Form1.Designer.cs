d05a85061df992bbcf7f2b00107e3ebe2e047e70
Gaurav Panchal <88033346+gaurav-codee@users.noreply.github.com>
2024-01-15 10:41:38 +0530

    Main Design Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Barcode_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtred = new System.Windows.Forms.TextBox();
+            this.txtyellow = new System.Windows.Forms.TextBox();
+            this.txtwhite = new System.Windows.Forms.TextBox();
+            this.txtblue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmdprint = new System.Windows.Forms.Button();
+            this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Barcode Generation System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(194, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(337, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time :";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(237, 76);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(80, 20);
+            this.datePicker.TabIndex = 3;
+            // 
+            // timePicker
+            // 
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(381, 76);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.Size = new System.Drawing.Size(80, 20);
+            this.timePicker.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(194, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Hospital Name ";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(314, 116);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 18);
+            this.txtName.TabIndex = 6;
+            this.txtName.Text = "IBMH";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(194, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Note :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(249, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Enter Value (0-10) KG";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(194, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Red";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(194, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Yellow";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(194, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "White";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(194, 284);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Blue";
+            // 
+            // txtred
+            // 
+            this.txtred.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtred.Location = new System.Drawing.Point(252, 184);
+            this.txtred.Name = "txtred";
+            this.txtred.Size = new System.Drawing.Size(42, 18);
+            this.txtred.TabIndex = 13;
+            // 
+            // txtyellow
+            // 
+            this.txtyellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtyellow.Location = new System.Drawing.Point(252, 213);
+            this.txtyellow.Name = "txtyellow";
+            this.txtyellow.Size = new System.Drawing.Size(42, 18);
+            this.txtyellow.TabIndex = 14;
+            // 
+            // txtwhite
+            // 
+            this.txtwhite.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtwhite.Location = new System.Drawing.Point(252, 250);
+            this.txtwhite.Name = "txtwhite";
+            this.txtwhite.Size = new System.Drawing.Size(42, 18);
+            this.txtwhite.TabIndex = 15;
+            // 
+            // txtblue
+            // 
+            this.txtblue.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtblue.Location = new System.Drawing.Point(252, 284);
+            this.txtblue.Name = "txtblue";
+            this.txtblue.Size = new System.Drawing.Size(42, 18);
+            this.txtblue.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.Location = new System.Drawing.Point(218, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 36);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Generate Barcode";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // cmdprint
+            // 
+            this.cmdprint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmdprint.Location = new System.Drawing.Point(300, 330);
+            this.cmdprint.Name = "cmdprint";
+            this.cmdprint.Size = new System.Drawing.Size(75, 37);
+            this.cmdprint.TabIndex = 21;
+            this.cmdprint.Text = "Print Barcode";
+            this.cmdprint.UseVisualStyleBackColor = false;
+            this.cmdprint.Click += new System.EventHandler(this.cmdprint_Click_1);
+            // 
+            // printDoc
+            // 
+            this.printDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDoc_PrintPage_1);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(565, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(956, 455);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmdprint);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtblue);
+            this.Controls.Add(this.txtwhite);
+            this.Controls.Add(this.txtyellow);
+            this.Controls.Add(this.txtred);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Barcode Generation System";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtred;
+        private System.Windows.Forms.TextBox txtyellow;
+        private System.Windows.Forms.TextBox txtwhite;
+        private System.Windows.Forms.TextBox txtblue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cmdprint;
+        private System.Drawing.Printing.PrintDocument printDoc;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Security.Policy;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BarcodeLib;
+
+
+namespace Barcode_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BarcodeLib.Barcode barcode = new BarcodeLib.Barcode();
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            SqlConnection conn = new SqlConnection("Data Source=GILSFDNGDSVR07;Initial Catalog=Nagda;User ID=sa;Password=Sa@12345");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Insert into BarcodeGen (Name,Red,Yellow,White,Blue,Date,Time,BarcodeRed,BarcodeYellow,BarcodeWhite,BarcodeBlue) values ('" + txtName.Text + "','" + txtred.Text + "','" + txtyellow.Text + "','" + txtwhite.Text + "','" + txtblue.Text + "','" + datePicker.Text + "','" + timePicker.Text + "','" + txtName.Text + " " + txtred.Text + "','" + txtName.Text + " " + txtyellow.Text + "','" + txtName.Text + " " + txtwhite.Text + "','" + txtName.Text + " " + txtblue.Text + "')", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Barcode generated successfully");
+            if (txtName.Text.Trim() == "")
+            {
+                MessageBox.Show("Input Barcode", "Msg", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtred.Text.Trim() == "")
+            {
+                MessageBox.Show("Input  Weight", "Msg", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtyellow.Text.Trim() == "")
+            {
+                MessageBox.Show("Input  Weight", "Msg", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtwhite.Text.Trim() == "")
+            {
+                MessageBox.Show("Input  Weight", "Msg", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtblue.Text.Trim() == "")
+            {
+                MessageBox.Show("Input  Weight", "Msg", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            errorProvider1.Clear();
+
+            BarcodeLib.TYPE type = BarcodeLib.TYPE.UNSPECIFIED;
+            type = BarcodeLib.TYPE.CODE128;
+           
+            try
+            {
+                if (type != BarcodeLib.TYPE.UNSPECIFIED)
+                {
+                    barcode.IncludeLabel = true;
+                    pictureBox1.Image = barcode.Encode(type, 
+                        txtName.Text + " R-" + txtred.Text , Color.Black, Color.White);
+                   
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Barcode Data Saved....");
+        }
+        private void printDoc_PrintPage_1(object sender, PrintPageEventArgs e)
+        {
+            using (Graphics graph = e.Graphics)
+            {
+                int rowY = 0;
+                for (int n3 = 0; n3 < 5; n3++)
+                {
+                    int rowX = 0;
+                    for (int nI = 0; nI < 8; nI++)
+                    {
+                        graph.DrawImage(pictureBox1.Image, rowY + 10, 5 + rowX);
+                        rowX = rowX + pictureBox1.Height + 20;
+                    }
+
+                    rowY = rowY + pictureBox1.Width + 22;
+
+
+
+                }
+               
+
+            }
+        }
+       
+        private void cmdprint_Click_1(object sender, EventArgs e)
+        {
+            printDoc.Print();
+
+        }
+
+
+            
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+    }
+
